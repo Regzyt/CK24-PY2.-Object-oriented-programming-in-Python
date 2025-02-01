f58c24f57f4be3eb498732575d544f1ae9d5129d
Ilnaz_Abitov <ilnazdom@mail.ru>
2025-02-01 16:43:52 +0300

    Add files via upload

---
+++ Лабараторная работа 3/Task_1.py
@@ -0,0 +1,55 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    def name(self) -> str:
+        return self._name
+
+    def author(self) -> str:
+        return self._author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook:
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self._pages = pages
+
+    def pages(self) -> int:
+        return self._pages
+
+    def pages(self, n_pages: int) -> None:
+        if not isinstance(n_pages, int):
+            raise TypeError("Укажите количество страниц типа int")
+        if n_pages <= 0:
+            raise ValueError("Укажите положительное число страниц")
+        self._pages = n_pages
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}), pages={self.pages!r}"
+
+
+class AudioBook:
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self._duration = duration
+
+    def duration(self) -> float:
+        return self._duration
+
+    def duration(self, n_duration: int) -> None:
+        if not isinstance(n_duration, float):
+            raise TypeError("Укажите продолжительность книги типа float")
+        if n_duration <= 0:
+            raise ValueError("Укажите положительное число продолжительности книги")
+        self._duration = n_duration
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}), duration={self.duration!r}"
